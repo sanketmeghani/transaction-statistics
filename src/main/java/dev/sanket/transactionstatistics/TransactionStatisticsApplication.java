d5cd24c7360ef3832e0a02713a429bd5b8dc011f
Sanket Meghani <sanket.meghani@gmail.com>
2018-05-12 11:49:57 +0530

    Initializing project using spring boot start scaffold

---
+++ src/main/java/dev/sanket/transactionstatistics/TransactionStatisticsApplication.java
@@ -0,0 +1,12 @@
+package dev.sanket.transactionstatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionStatisticsApplication.class, args);
+	}
+}
