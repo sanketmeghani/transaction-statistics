210c1bcd1264ea8e7220287bde402b2825e9d722
Sanket Meghani <sanket.meghani@gmail.com>
2018-05-13 00:25:50 +0530

    Using custom executor pool for executing tasks

--- src/main/java/dev/sanket/transactionstatistics/TransactionStatisticsApplication.java
+++ src/main/java/dev/sanket/transactionstatistics/TransactionStatisticsApplication.java
@@ -1,14 +1,30 @@
 package dev.sanket.transactionstatistics;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 @SpringBootApplication
 @EnableScheduling
-public class TransactionStatisticsApplication {
+public class TransactionStatisticsApplication implements SchedulingConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(TransactionStatisticsApplication.class, args);
+    }
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(taskExecutor());
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    public Executor taskExecutor() {
+        return Executors.newScheduledThreadPool(1);
     }
 }
