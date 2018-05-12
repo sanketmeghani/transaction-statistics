7ffec199349582c3c4729f7f3890364788a3e909
Sanket Meghani <sanket.meghani@gmail.com>
2018-05-12 15:28:19 +0530

    Returning dummy statistics

--- src/main/java/dev/sanket/transactionstatistics/controller/StatisticsController.java
+++ src/main/java/dev/sanket/transactionstatistics/controller/StatisticsController.java
@@ -1,5 +1,7 @@
 package dev.sanket.transactionstatistics.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +13,13 @@ import dev.sanket.transactionstatistics.model.Statistics;
 @RestController("/statistics")
 public class StatisticsController {
 
+    private static final Logger logger = LoggerFactory.getLogger(StatisticsController.class);
+
     @RequestMapping(method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
     public ResponseEntity<Statistics> getStatistics() {
-        return ResponseEntity.ok(new Statistics());
+
+        Statistics statistics = new Statistics();
+        logger.info("Returning statistics - {}", statistics);
+        return ResponseEntity.ok(statistics);
     }
 }
