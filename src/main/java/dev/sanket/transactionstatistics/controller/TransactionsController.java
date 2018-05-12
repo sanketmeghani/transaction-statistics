560da46d94776d2a3c857765c808f7165fbc30f2
Sanket Meghani <sanket.meghani@gmail.com>
2018-05-12 13:06:59 +0530

    Adding transactions model and return code 201 for transactions api

--- src/main/java/dev/sanket/transactionstatistics/controller/TransactionsController.java
+++ src/main/java/dev/sanket/transactionstatistics/controller/TransactionsController.java
@@ -1,15 +1,20 @@
 package dev.sanket.transactionstatistics.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import dev.sanket.transactionstatistics.model.Transaction;
 
 @RestController("/transactions")
 public class TransactionsController {
 
     @RequestMapping(method = RequestMethod.POST, consumes = { MediaType.APPLICATION_JSON_VALUE })
-    public String submitTransaction() {
-        return "Hello";
+    public ResponseEntity<String> submitTransaction(@RequestBody Transaction transaction) {
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
